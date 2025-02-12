a5d26cef341801dd9cb345d00db3f1a2ff1c101f
vidishraj <vidishraj@gmail.com>
2025-02-12 18:40:59 +0530

    maximum-depth-of-binary-tree
    Runtime: 36 ms (5.6463999999999945 percentile)
    Memory: 17656000 bytes (100.0 percentile)
    
    /Users/vidishraj/Desktop/Leetcode_History/maximum-depth-of-binary-tree.py

---
+++ maximum-depth-of-binary-tree.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    
+    def rec(self, node):
+        if node is None:
+            return 0
+        return max(1+self.rec(node.right, ), 1+self.rec(node.left))
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        return self.rec(root)
+            
