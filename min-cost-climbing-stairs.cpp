ca843d75b92a4631db1f82f65d4cd3c142032d2e
vidishraj <vidishraj@gmail.com>
2025-02-13 14:34:04 +0530

    min-cost-climbing-stairs
    Runtime: 4 ms (6.7785999999999955 percentile)
    Memory: 13604000 bytes (100.0 percentile)
    
    /Users/vidishraj/Desktop/Leetcode_History/min-cost-climbing-stairs.cpp

---
+++ min-cost-climbing-stairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int dp[cost.size()+1];
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+        for(int i=2;i<=cost.size();i++){
+            if(i==cost.size()){
+                dp[i]=min(dp[i-1], dp[i-2]);
+            }
+            else{
+                dp[i]=min(dp[i-1]+cost[i], dp[i-2]+cost[i]);
+            }
+        }
+        return dp[cost.size()];
+
+    }
+};
